ba7fc92537144600573e15964ee5d14378dbe3cd
LazarevY <lazarev@cs.vsu.ru>
2021-03-04 23:15:14 +0300

    Start implement logic class

--- Lab2Dir/Logic/logic.cpp
+++ Lab2Dir/Logic/logic.cpp
@@ -1,6 +1,39 @@
 #include "logic.h"
 
-Logic::Logic()
+Logic::Logic(Field *field) :
+    m_field(field)
 {
 
+}
+
+void Logic::addObject(BaseObject *o, const IntegerVector &position)
+{
+    o->setId(m_idCounter++);
+    m_objectsMap[o->id()] = o;
+    m_field->add(position, o);
+}
+
+void Logic::addObject(BaseObject *o)
+{
+    addObject(o, o->position());
+}
+
+void Logic::moveObject(BaseObject *o, const IntegerVector &toCell)
+{
+    if (!m_objectsMap.contains(o->id())){
+        return;
+    }
+
+    m_field->move(toCell, o);
+
+}
+
+void Logic::removeObject(BaseObject *o)
+{
+    if (!m_objectsMap.contains(o->id())){
+        return;
+    }
+
+    m_objectsMap.take(o->id());
+    m_field->remove(o);
 }
--- Lab2Dir/Logic/logic.h
+++ Lab2Dir/Logic/logic.h
@@ -1,11 +1,60 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+#include <QMap>
+
+#include "field.h"
+#include "Objects/baseobject.h"
 
 class Logic
 {
 public:
-    Logic();
+    Logic(Field *field);
+
+    template<typename ObjectType>
+    QVector<ObjectType *> getAllObjectsInCell(const IntegerVector &coords){
+        auto objects = getAllObjectsInCell<BaseObject>(coords);
+        return fillterByType<BaseObject, ObjectType>(objects);
+    }
+
+
+    template<typename ObjectType>
+    QVector<ObjectType *> getAll(){
+        return fillterByType<BaseObject, ObjectType>(m_objectsMap.values().toVector());
+    }
+
+
+   void addObject(BaseObject *o, const IntegerVector &position);
+   void addObject(BaseObject *o);
+   void moveObject(BaseObject *o, const IntegerVector &toCell);
+   void removeObject(BaseObject *o);
+
+private:
+    template<typename P, typename D>
+    QVector<D *> fillterByType(const QVector<P*> in){
+        QVector<D *> filetered = QVector<D *>();
+                for (auto o: in){
+                    D * casted  = dynamic_cast<D *>(o);
+                    if (casted)
+                        filetered.append(casted);
+                }
+                return filetered;
+    }
+
+private:
+    Field *m_field;
+    QMap<int, BaseObject *> m_objectsMap;
+    int m_idCounter = 1;
 };
+
+template<>
+inline QVector<BaseObject *> Logic::getAllObjectsInCell<BaseObject>(const IntegerVector &coords){
+    return m_field->operator()(coords).getAllObjects();
+}
+
+template<>
+inline QVector<BaseObject *> Logic::getAll<BaseObject>(){
+    return m_objectsMap.values().toVector();
+}
 
 #endif // LOGIC_H
