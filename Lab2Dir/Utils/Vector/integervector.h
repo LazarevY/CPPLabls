ad1a4d79a123c0c58f89676d67c97246f0b42087
LazarevY <lazarev@cs.vsu.ru>
2021-03-08 00:35:47 +0300

    add qMin func for Integer Vector

--- Lab2Dir/Utils/Vector/integervector.h
+++ Lab2Dir/Utils/Vector/integervector.h
@@ -1,6 +1,7 @@
 #ifndef INTEGERVECTOR_H
 #define INTEGERVECTOR_H
 
+#include <QtGlobal>
 
 class IntegerVector
 {
@@ -31,5 +32,10 @@ public:
 private:
     int m_x, m_y;
 };
+
+template <>
+inline const IntegerVector &qMin<IntegerVector>(const IntegerVector &a, const IntegerVector &b){
+    return a.lenghtSqr() < b.lenghtSqr()? a : b;
+}
 
 #endif // INTEGERVECTOR_H
